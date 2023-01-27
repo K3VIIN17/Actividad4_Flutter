c120687f126ed999f739e0437052e81c4381f750
Kevin Rafael Guzmán Arjona <116427318+K3VIIN17@users.noreply.github.com>
2023-01-27 01:40:31 -0600

    subir

---
+++ lib/screen/pantalla1.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/screen/pantalla2.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
+          child: const Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
